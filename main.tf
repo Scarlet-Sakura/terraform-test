1e47eea4b88a0c29b03581e5b912a712f84d70ea
Scarlet-Sakura <danai231@windowslive.com>
2024-02-26 22:34:49 +0100

    autoscalling corrections

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ module "blog_vpc" {
 }
 
 
-module "asg" {
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
 
@@ -42,7 +42,7 @@ module "asg" {
   max_size = 2
 
   vpc_zone_identifier =  module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups = [module.blog_sg.security_group_id]
 
   image_id           = data.aws_ami.app_ami.id
